32c7454c3b1c4762be683541b7563a5ffb5663fa
KacperWojas <146821452+Cre4to5@users.noreply.github.com>
2024-10-10 21:59:55 +0200

    polybius square done

--- ciphers.py
+++ ciphers.py
@@ -13,21 +13,39 @@ def polybius_square_cipher(to_process, key, mode):
     a = int(key[35])
     b = int(key[36])
     temp = ""
+    temp1 = ""
     if not mode:
         for l in to_process:
             index = table.find(l)
             temp += f"{index // 7 + 1}{index % 7 + 1}"
-        for l in [temp[i:i+5] for i in range (0, len(temp), 5)]:
+        for l in chop(temp,5):
             while len(l)<5:
-                l += f"{random.randint(1,7)}"
-            temp += f"{int(l)*int(l)*a+b} "
+                l += f"{random.randint(8,9)}"
+            # print(l)
+            # print(int(l))
+            # print(int(l)*int(l))
+            # print(a)
+            # print(int(l)*int(l)*a)
+            # print(b)
+            # print(int(l)*int(l)*a+b)
+            temp1 += f"{int(l)*int(l)*a+b} "
     else:
         for l in to_process.split(" "):
-            (math.sqrt(int(l)/a)-b)#TODO
-    return temp
+            # print(l)
+            # print(int(l))
+            # print(a)
+            # print(int(l)/a)
+            temp += str(int(math.sqrt((int(l)-b)/a)))
+        print(temp)
+        for cords in chop(temp,2):
+            if len(cords) == 2 and int(cords[0]) - 1 < 5 and int(cords[1]) - 1 < 7:
+                temp1 += key[(int(cords[0])-1) % 5 * 7 + int(cords[1]) - 1]
+    return temp1
 def polybius_key():
     return f"{random.shuffle(ALPHABET)}{random.randint(1,9)}{random.randint(1,9)}"
 
+def chop(string, interval):
+    return [string[i:i+interval] for i in range (0, len(string), interval)]
 def message_cleaner(msg):
     temp = ""
     for l in msg.lower():
